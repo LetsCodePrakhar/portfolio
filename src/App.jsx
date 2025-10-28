585bda957e0404caeb3cb6589d15961ba3061d96
codeXp1908 <prakharsaxena3416@gmail.com>
2025-10-29 02:24:53 +0530

    Added HomePage

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Navbar id="nav_bar"></Navbar>
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import BlurText from "../utils/BlurText";
+
+const Home = () => {
+  const handleAnimationComplete = () => {
+    console.log("Animation completed!");
+  };
+
+  return (
+    <>
+      <div className="home_Container" id="home">
+        <div className="left_Section">
+          <h3 className="stroke_Text">Hi,</h3>
+          <h3 className="stroke_Text">Myself</h3>
+          <BlurText
+            text="Prakhar Saxena"
+            delay={150}
+            animateBy="words"
+            direction="top"
+            onAnimationComplete={handleAnimationComplete}
+          />
+
+          <p>
+            Frontend Developer <i class="fa-solid fa-location-dot"></i>Madhya
+            Pradesh , (IN)
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
